1f3d36ab4bc916f661e06ed599f3a46f72befc0f
Martin Veselinov <martinvveselinov@gmail.com>
2022-01-11 00:06:58 +0200

    Initial commit

---
+++ comments.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+if (isset($_SESSION["id"]) == false) {
+  header("Location:login.php");
+}
+
+if (isset($_SESSION["admin"]) == true) {
+  $comment = htmlspecialchars($_POST["comment"]);
+  $projId = $_POST["projectId"];
+  $usrId = $_SESSION["id"];
+  $date = date('Y-m-d H:i:s');
+  
+  require 'db_setup.php';
+  try {
+    $conn = new PDO(
+		"mysql:host=$serverName;dbname=$database;",
+		$user,
+		$pass
+	  );
+	
+	$sql = "INSERT INTO `comments` (`ProjectId`,`UserId`,`Text`,`Timestamp`) VALUES ('$projId','$usrId','$comment','$date')";
+	$sth = $conn->prepare($sql);
+	$sth->execute();
+
+	} catch (PDOException $e) {
+	  echo "Error: " . $e->getMessage();
+  }
+  $conn = null;
+}
+header("Location:project.php?id=$projId");
+?>
