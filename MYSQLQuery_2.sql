361f78bf39f89a0f211a65d3a1d2e247ad6dbd9b
stefanap98 <84773438+stefanap98@users.noreply.github.com>
2022-01-19 01:43:57 +0200

    Added second database

---
+++ MYSQLQuery_2.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS piechart;
+Use piechart;
+
+CREATE TABLE IF NOT EXISTS ProjectGrades  (
+  Id int NOT NULL PRIMARY KEY,
+  Grade int UNSIGNED DEFAULT 0
+);
+
+INSERT INTO ProjectGrades (Id, Grade) 
+VALUES ('1','2')  ,  ('2','3') , ('3','4') , ('4','5') , ('5','6');
